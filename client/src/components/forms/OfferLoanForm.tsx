086c5babee29ac565e06d750219c8f6dd01c64c6
Divesh Kumar S <diveshkumar.s007@gmail.com>
2025-07-26 19:19:46 +0530

    Updated OfferLoanForm.tsx

--- client/src/components/forms/OfferLoanForm.tsx
+++ client/src/components/forms/OfferLoanForm.tsx
@@ -227,4 +227,8 @@ export function OfferLoanForm({ isOpen, onClose, onSuccess }: OfferLoanFormProps
           </form>
         </Form>
       </DialogContent>
+              )
+              }
+
+  )
 }
